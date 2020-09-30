1d21024d6b9b88598b5d347b13acac31cc59c751
Amann Malik <amannmalik@gmail.com>
2020-09-30 16:49:30 -0500

    initialize

---
+++ src/main/java/systems/cauldron/utility/trading/Application.java
@@ -0,0 +1,9 @@
+package systems.cauldron.utility.trading;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hey");
+    }
+
+}
---
+++ src/test/java/systems/cauldron/utility/trading/BasicTest.java
@@ -0,0 +1,12 @@
+package systems.cauldron.utility.trading;
+
+import org.junit.jupiter.api.Test;
+
+public class BasicTest {
+
+    @Test
+    public void test() {
+        Application.main(new String[0]);
+    }
+
+}
